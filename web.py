17e34ae5d6be6e7e21165ca0bc3481f13b763795
Administrator <Administrator@SC-201911121327>
2020-11-14 19:40:57 -0500

    Initial Commit

---
+++ web.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/bulletin')
+def bulletin():
+    bulletin_id = request.args.get('bulletin_id')
+    file = 'bulletin.html'
+    if bulletin_id is not None:
+        file = "bulletin/" + bulletin_id + ".html"
+    return render_template(file)
+
+@app.route('/intro')
+def intro():
+    return render_template("intro.html")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run()
